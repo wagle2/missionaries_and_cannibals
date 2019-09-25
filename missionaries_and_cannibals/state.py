3728b2d4e926fbd9bf69a9092f565a3fa297c3ae
wagle2 <archmage.maple@gmail.com>
2019-09-25 14:27:50 +0900

    h

--- missionaries_and_cannibals/state.py
+++ missionaries_and_cannibals/state.py
@@ -1,13 +1,19 @@
 import operator
 
-class State :
+
+class State:
     def __init__(self, state, action):
         self.state = state
         self.action = action
 
-    def move(self) :
+    def move(self):
         pass
+                
 
-    def isActionValid(self) :
-        pass
-        
+    def isActionValid(self):
+        if self.state[2] != self.action[2]:
+            return False
+        elif self.state[0] < self.state[1]:
+            return False
+        elif (self.action[0] + self.action[1] >= 2 | | self.action[0]+self.action[1] == 0):
+            return False
